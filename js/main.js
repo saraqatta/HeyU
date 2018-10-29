f41752fcbdd9958fd3467196b140cd9e11fed5d6
saraqatta <32012154+saraqatta@users.noreply.github.com>
2018-10-29 11:07:57 +0200

    2
    2

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ $(document).ready(function(){
     $(window).scroll(function(){
         scrollTop = $(window).scrollTop();
         if (scrollTop > 0){
+			$('.top').show(500);
             $('.navbar').addClass ('scrollNav');
         }else if (scrollTop == 0){
+			$('.top').hide(500);
             $('.navbar').removeClass('scrollNav');
         }
     });
+
+$('.top').click(function(){
+$('body, html').animate({
+scrollTop:0}, 500);
+});
+
+
+
 
 });
