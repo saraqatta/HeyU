c92c41fe9a699a3b3f38c94c79409dddde7954bd
saraqatta <32012154+saraqatta@users.noreply.github.com>
2018-10-29 09:45:23 +0200

    1
    1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var scrollTop =0;
+    $(window).scroll(function(){
+        scrollTop = $(window).scrollTop();
+        if (scrollTop > 0){
+            $('.navbar').addClass ('scrollNav');
+        }else if (scrollTop == 0){
+            $('.navbar').removeClass('scrollNav');
+        }
+    });
+
+});
